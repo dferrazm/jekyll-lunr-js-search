9ee29594428605d03b53548d12e86d62007b651f
Daniel Ferraz <d.ferrazm@gmail.com>
2015-11-01 17:03:15 +0100

    Fix page renderer to avoid build errors when dealing with pages that has layouts set through the config file (default values)

--- lib/jekyll_lunr_js_search/page_renderer.rb
+++ lib/jekyll_lunr_js_search/page_renderer.rb
@@ -6,15 +6,17 @@ module Jekyll
       def initialize(site)
         @site = site
       end
-      
+
       # render the item, parse the output and get all text inside <p> elements
       def render(item)
         layoutless = item.dup
-        layoutless.data = layoutless.data.dup
-        layoutless.data.delete('layout')
+        # # return new hash based on the item's data but without the layout
+        layoutless.data = item.data.select do |k,_|
+          k != 'layout'
+        end
         layoutless.render({}, @site.site_payload)
         Nokogiri::HTML(layoutless.output).text
       end
     end
-  end  
+  end
 end
